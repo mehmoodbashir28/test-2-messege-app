8179a6597172cc9185320e0aa8f03d293ddaef50
Mehmood Bashir <39158675+mehmoodbashir28@users.noreply.github.com>
2018-10-08 17:48:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    alert("your Message has been sent!");
+    window.location.reload(); 
+}
+
+// let string = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
+// let stringAbc = ``
+// function count() {
+//     for (let i=0; i < 26; i++) {
+//          document.querySelector(`.buttons`).innerHTML +=  onclick="valueInput('${alpha[i]}')">
